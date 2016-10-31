2c2436b981cfba0704a63fa0268f03876ce62a75
Kevin Leong <kgleong@squareup.com>
2016-10-30 18:14:43 -0700

    Initial Commit

---
+++ GithubClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubClient
+//
+//  Created by Kevin Leong on 10/30/16.
+//  Copyright © 2016 Orangemako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
