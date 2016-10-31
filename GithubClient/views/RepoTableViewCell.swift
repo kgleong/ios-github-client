7ae445bcf43c9550b58737aa57df47022e4000af
Kevin Leong <kgleong@squareup.com>
2016-10-30 18:55:29 -0700

    Add table view cell.

---
+++ GithubClient/views/RepoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepoTableViewCell.swift
+//  GithubClient
+//
+//  Created by Kevin Leong on 10/30/16.
+//  Copyright © 2016 Orangemako. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
