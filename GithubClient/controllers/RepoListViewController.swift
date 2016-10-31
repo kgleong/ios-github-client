7f2bd1f5fe47eef3b8aa298e3541ea22b9a520d2
Kevin Leong <kgleong@squareup.com>
2016-10-30 18:45:37 -0700

    Initial commit.

---
+++ GithubClient/controllers/RepoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RepoListViewController.swift
+//  GithubClient
+//
+//  Created by Kevin Leong on 10/30/16.
+//  Copyright © 2016 Orangemako. All rights reserved.
+//
+
+import UIKit
+
+class RepoListViewController: UIViewController {
+    
+    let navigationTitle = "Repos"
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = navigationTitle
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'GithubClient' do
+  pod 'Alamofire', '~> 4.0'
+  pod 'AFNetworking', '~> 3.0'
+  pod 'MBProgressHUD', '~> 1.0.0'
+end
